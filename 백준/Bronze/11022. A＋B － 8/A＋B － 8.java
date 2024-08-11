01463e758e82ccc3670811ae275a083b2a3868c3
xuv2 <104479096+xub2@users.noreply.github.com>
2024-08-11 18:36:56 +0900

    [Bronze V] Title: A+B - 8, Time: 272 ms, Memory: 19068 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 1 ; i <= T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = a+b;
+
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + c);
+        }
+        sc.close();
+
+    }
+}
