9745a7b363645a9f7b70a4956d6274eca5f3876a
xuv2 <104479096+xub2@users.noreply.github.com>
2024-08-13 17:06:28 +0900

    [Bronze IV] Title: 기찍 N, Time: 1128 ms, Memory: 33548 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = N; ; i--){
+            System.out.println(i);
+            if (i==1)
+                break;
+        }
+        sc.close();
+    }
+}
