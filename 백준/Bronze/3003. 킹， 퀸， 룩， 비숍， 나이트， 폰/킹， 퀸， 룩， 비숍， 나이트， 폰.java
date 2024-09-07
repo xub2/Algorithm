8614718f4e192b67a6df81b625863387519b8889
xub2 <104479096+xub2@users.noreply.github.com>
2024-09-08 01:45:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 168 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -7,19 +7,21 @@ public class Main{
 
         int whiteChess[] = {1,1,2,2,2,8}; // 킹, 퀸, 룩, 비숍, 나이트, 폰 개수
 
-        int whiteChessInput[] = new int[6]; //동혁이가 찾은 체스말을 저장
+        int whiteChessInput[] = new int[whiteChess.length]; //동혁이가 찾은 체스말을 저장
 
-        for(int i = 0 ; i < 6 ; i++){ // 여기서 동혁이가 찾은 체스말을 입력받음
+        for(int i = 0 ; i < whiteChess.length ; i++){ // 여기서 동혁이가 찾은 체스말을 입력받음
             whiteChessInput[i] = sc.nextInt();
         }
 
-        for(int i = 0 ; i < 6; i++){
+        for(int i = 0 ; i < whiteChess.length; i++){
             if(whiteChess[i] == whiteChessInput[i]){
                 System.out.println(0);
             }
             else{
                 System.out.println(whiteChess[i] - whiteChessInput[i]);
             }
+        }
+            
 
         sc.close();
 
