9206a7ac3b8766e77ecb5e1382942908ea3e03d3
xuv2 <104479096+xub2@users.noreply.github.com>
2025-12-31 13:03:36 +0900

    [level 2] Title: 방문 길이, Time: 17.85 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+     private static boolean isValidArea(int x, int y) {
+        return 0 <= x && x < 11 && 0 <= y && y < 11;
+    }
+
+    private static final HashMap<Character, int[]> location = new HashMap<>();
+
+    private static void initLocation() {
+        location.put('U', new int[]{0, 1});
+        location.put('D', new int[]{0, -1});
+        location.put('L', new int[]{-1, 0});
+        location.put('R', new int[]{1, 0});
+    }
+
+    public int solution(String dirs) {
+        initLocation();
+        int x = 5, y = 5;
+        Set<String> answer = new HashSet<>();
+
+        for (int i = 0; i < dirs.length(); i++) {
+            int[] offset = location.get(dirs.charAt(i));
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+
+            if (!isValidArea(nx, ny)) {
+                continue;
+            }
+
+            // 출발지 도착지 도착지 출발지
+            answer.add(x + " " + y + " " + nx + " " + ny);
+            answer.add(nx + " " + ny + " " + x + " " + y);
+
+            x = nx;
+            y = ny;
+        }
+
+        return answer.size() / 2;
+    }
+}
