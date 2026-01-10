e80a8dbb394f6bcdd91e9292641ac7eaa7be4633
xuv2 <104479096+xub2@users.noreply.github.com>
2026-01-10 22:03:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.97 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String string : completion) {
+            map.put(string, map.getOrDefault(string, 0) + 1);
+        }
+
+        for (String string : participant) {
+            if (map.getOrDefault(string, 0) == 0) {
+                return string;
+            }
+
+            map.put(string, map.get(string) - 1);
+        }
+
+        return null;
+    }
+        
+}
