8bbd6823da5473b7da7d0962608e9020f6a50978
xuv2 <104479096+xub2@users.noreply.github.com>
2026-02-02 13:10:36 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 760 ms, Memory: 98708 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private final static int MAX = 1000000 + 10;
+    private static ArrayList<Integer>[] graph; // Integer을 담을 리스트를 담을 배열
+    private static boolean[] visited;
+    private static int N;
+    private static int M;
+    private static int R;
+    private static int[] answer;
+    private static int order; // 몇번째로 방문헀는지 카운트할 변수
+
+    private static void dfs(int idx) {
+        visited[idx] = true;
+        answer[idx] = order;
+        order++;
+
+        for (int i = 0; i < graph[idx].size(); i++) {
+            int next = graph[idx].get(i);
+            if (visited[next] == false) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        // 1. 그래프 연결정보 채우기
+        graph = new ArrayList[MAX];
+        for (int i = 1; i <= N; i++) { // 1 ~ 5까지
+            graph[i] = new ArrayList<>();
+        }
+        visited = new boolean[MAX];
+        answer = new int[MAX];
+        order = 1;
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+
+        // 2. 오름차순 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        // 3. dfs 호출
+        dfs(R);
+
+        // 4. 출력
+        for (int i = 1; i <= N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+        bw.close();
+        br.close();
+
+    }
+}
