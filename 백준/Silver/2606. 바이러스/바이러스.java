af34619d1a047f6f922d3070170cf47736705191
xuv2 <104479096+xub2@users.noreply.github.com>
2026-02-02 15:06:58 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,57 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    private static boolean[][] graph;
+    private static ArrayList<Integer>[] graph;
     private static boolean[] visited;
-    private static int N,M;
-    private static int answer;
+    private static int N,M; // 노드 개수 N , 간선 개수 M
+    private static int count;
 
-    private static void dfs(int idx) {
-        visited[idx] = true;
-        answer++;
+    private static void dfs(int index) {
+        visited[index] = true;
 
-        for (int i = 0; i <= N; i++) {
-            if (visited[i] == false && graph[idx][i]) {
-                dfs(i);
+        for (int i = 0; i < graph[index].size(); i++) {
+            int next = graph[index].get(i);
+
+            if(visited[next] == false){ // 방문하지 않고, 간선이 있어야
+                count++;
+                dfs(graph[index].get(i));
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine()); // 노드 개수
+        M = Integer.parseInt(br.readLine()); // 긴선 개수
 
-        graph = new boolean[N + 1][N + 1];
+        // 1. 그래프 구현
+        graph = new ArrayList[N + 1];
         visited = new boolean[N + 1];
+        count = 0;
 
-        // 1. 그래프 채우기
-        int x;
-        int y;
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
+        int x , y;
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             x = Integer.parseInt(st.nextToken());
             y = Integer.parseInt(st.nextToken());
-            graph[x][y] = true;
-            graph[y][x] = true;
+
+            graph[x].add(y);
+            graph[y].add(x);
         }
 
-        // 2. 재귀 호출
         dfs(1);
 
-        // 3. 출력
-        bw.write(String.valueOf(answer - 1));
+        bw.write(String.valueOf(count));
         br.close();
         bw.close();
     }
+
 }
