6f4466a866fac336b30a54e459fd9cbf847ed028
Yosup Lim <104479096+xub2@users.noreply.github.com>
2026-06-17 14:28:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.45 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,63 +1,72 @@
 import java.util.*;
 
 class Solution {
-    final int MAX = 100 + 10;
-
-    boolean[][] graph;
-    boolean[][] visited;
-    int answer = -1;
-
+    
+    private static final int MAX = 100 + 10;
+    
+    private static boolean[][] graph;
+    private static boolean[][] visited;
+    
+    private static int[][] dist;
+    
     // 상 하 좌 우
-    int[] dirY = {-1, 1, 0, 0};
-    int[] dirX = {0, 0, -1, 1};
-
-    public void bfs(int y, int x, int count, int targetY, int targetX) {
-
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{y, x, count});
-        visited[y][x] = true;
-
-
-        while (!queue.isEmpty()) {
-            int[] polled = queue.poll();
-            int pollY = polled[0];
-            int pollX = polled[1];
-            int curDist = polled[2];
-
-            if (pollY == targetY && pollX == targetX) {
-                this.answer = curDist;
-                return;
-            }
-
-            for (int i = 0; i < dirY.length; i++) {
-                int newY = pollY + dirY[i];
-                int newX = pollX + dirX[i];
-
-                if (graph[newY][newX] && !visited[newY][newX]) {
-                    visited[newY][newX] = true;
-                    queue.offer(new int[]{newY, newX, curDist + 1});
+    private static int[] dirY = {-1, 1, 0, 0};
+    private static int[] dirX = {0, 0, -1, 1};
+    
+    private static Deque<int[]> queue;
+    
+    private static int bfs(int[] idx, int[] end){
+        dist = new int[MAX][MAX];
+        queue = new ArrayDeque<>();
+        queue.offer(idx);
+        
+        visited[idx[0]][idx[1]] = true;
+        dist[idx[0]][idx[1]] = 1;
+        
+        while(!queue.isEmpty()){
+            int[] next = queue.poll();
+            
+            for(int i = 0 ; i < 4; i++){
+                int nextY = next[0] + dirY[i];
+                int nextX = next[1] + dirX[i];
+                
+                if(!visited[nextY][nextX] && graph[nextY][nextX]){
+                    visited[nextY][nextX] = true;
+                    dist[nextY][nextX] = dist[next[0]][next[1]] + 1;
+                    
+                    if(nextY == end[0] && nextX == end[1]){
+                        return dist[nextY][nextX];
+                    }
+                    
+                    queue.offer(new int[] {nextY, nextX});
                 }
+
             }
-
         }
-
+        
+        // 방문 다 하고 마지막 종점 visited = false 면 return -1;
+        if(!visited[end[0]][end[1]]){
+            return -1;
+        }
+        
+        return dist[end[0]][end[1]];
     }
-
+    
     public int solution(int[][] maps) {
-
+        
         graph = new boolean[MAX][MAX];
         visited = new boolean[MAX][MAX];
-
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[i].length; j++) {
-                if (maps[i][j] == 1) {
+        
+        for(int i = 0 ; i < maps.length; i++){
+            for(int j = 0; j < maps[i].length; j++){
+                
+                // 0이면 벽 1이면 길
+                if(maps[i][j] == 1){
                     graph[i + 1][j + 1] = true;
                 }
             }
         }
-
-        bfs(1, 1, 1, maps.length, maps[0].length);
-
-        return answer;
+        
+        return bfs(new int[]{1,1}, new int[]{maps.length, maps[0].length});
     }
 }
