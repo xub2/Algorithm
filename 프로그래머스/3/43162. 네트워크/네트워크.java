217c54e0f111a82019857f7df14c13cc3c90164e
Yosup Lim <104479096+xub2@users.noreply.github.com>
2026-06-17 15:10:16 +0900

    [level 3] Title: 네트워크, Time: 0.55 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,46 +2,44 @@ import java.util.*;
 
 class Solution {
     
-    ArrayList<Integer>[] graph;
-    boolean[] visited;
+    private static final int MAX = 200 + 10;
+    
+    private static boolean[][] graph;
+    private static boolean[] visited;
+    
+    private static void dfs(int idx){
+        visited[idx] = true;
+        
+        for(int i = 0 ; i < graph[idx].length; i++){
+            if(!visited[i] && graph[idx][i]){
+                dfs(i);
+            }
+        }
+    }
     
     public int solution(int n, int[][] computers) {
-        graph = new ArrayList[n];
-        visited = new boolean[n];
-
-        for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    graph[i].add(j);
+        graph = new boolean[MAX][MAX];
+        visited = new boolean[MAX];
+        
+        for(int i = 0; i < computers.length; i++){
+            for(int j = 0 ; j < computers[i].length; j++){
+                if(computers[i][j] == 1){
+                    graph[i + 1][j + 1] = true;
                 }
             }
         }
-
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                count++;
+        
+        int answer = 0;
+        
+        // 전체에 대해 DFS 수행 
+        for(int i = 0 ; i < n ; i++){
+            if(!visited[i + 1]){
+                dfs(i + 1);
+                answer++;
             }
+            
         }
-
-        return count;
-    }
-    
-    public void dfs(int idx) {
-        visited[idx] = true;
-
-        for (int i = 0; i < graph[idx].size(); i++) {
-            int next = graph[idx].get(i);
-
-            if (!visited[next]) {
-                dfs(next);
-            }
-        }
+        
+        return answer;
     }
 }
